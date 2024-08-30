4709bf4903322d3078f00739b510ce4326bc3e42
Asher <aantrim246@cedarville.edu>
2024-08-30 14:11:58 -0400

    Initial Commit

---
+++ Lab02Tests/Lab02Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Lab02Tests.swift
+//  Lab02Tests
+//
+//  Created by Asher Antrim on 8/30/24.
+//
+
+import Testing
+@testable import Lab02
+
+struct Lab02Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
