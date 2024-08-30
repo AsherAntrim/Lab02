56e454e65504d4c1768ea382f44e910241e9236d
Asher <aantrim246@cedarville.edu>
2024-08-30 14:42:20 -0400

    i think I finished with lab

--- Lab02/ContentView.swift
+++ Lab02/ContentView.swift
@@ -10,14 +10,83 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Choose")
+                .fontWeight(.black)
+                .font(.largeTitle)
+                .bold()
+            Text("Your Plan")
+                .fontWeight(.black)
+                .font(.largeTitle)
+                .bold()
+            ZStack{
+                HStack {
+                    PricingView()
+                        .foregroundStyle(.purple)
+                    Spacer()
+                        .frame(width: 60)
+                    PricingView()
+                        .foregroundStyle(.mint)
+                }
+                HStack {
+                    VStack{
+                        Text ("Basic")
+                            .fontWeight(.black)
+                            .font(.largeTitle)
+                            .bold()
+                            .foregroundStyle(.white)
+                        Text ("$9")
+                            .fontWeight(.black)
+                            .font(.largeTitle)
+                            .bold()
+                            .foregroundStyle(.white)
+                        Text ("per month")
+                            .font(.headline)
+                            .foregroundStyle(.white)
+                    }
+                    Spacer()
+                        .frame(width: 120)
+                    VStack{
+                        Text ("Pro")
+                            .fontWeight(.black)
+                            .font(.largeTitle)
+                            .bold()
+                        Text ("$19")
+                            .fontWeight(.black)
+                            .font(.largeTitle)
+                            .bold()
+                        Text ("per month")
+                            .font(.headline)
+                    }
+                }
+            }
+            ZStack{
+                PricingView()
+                    .foregroundStyle(.yellow)
+                VStack {
+                    Text ("Team")
+                        .fontWeight(.black)
+                        .font(.largeTitle)
+                        .bold()
+                    Text ("$199")
+                        .fontWeight(.black)
+                        .font(.largeTitle)
+                        .bold()
+                    Text ("per month")
+                        .font(.headline)
+                }
+            }
+            
         }
         .padding()
     }
 }
+
+struct PricingView: View {
+var body: some View {
+    RoundedRectangle(cornerRadius: 20)
+    }
+}
+
 
 #Preview {
     ContentView()
